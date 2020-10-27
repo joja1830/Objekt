4879bee375370c22514b7e4dc40f846c3d304bb6
josefine jacobsen <josejaco@live.dk>
2020-10-27 13:26:16 +0100

    objekter_privat or public in class

---
+++ Objekt Orienteret Programmering/objekt_class public or privat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace objekt_class_public_or_privat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake pices = new Cake();
+            Console.WriteLine("I have mad a " + pices.cakeName);
+            Console.WriteLine("the cake is mad with " + pices.cakeButterType + "on top");
+            //Console.WriteLine("there is" + pices.cakePicesLeft + " on");
+            Console.WriteLine("there are " + pices.cakeCandls + " candls on the cake");
+        }   
+    }
+    public class Cake
+    {
+        public string cakeName = "Birthday Cake";
+        private int cakePieces = 20;
+        public string cakeButterType = "Vanilla Butter";
+        public int cakeCandls = 25;
+
+        public string cakePicesLeft()
+        {
+            this.cakePieces = cakePieces - 1;
+            return string.Format("there is now {0} pieces left\n", cakePieces);
+            
+        }
+     
+    }
+}
