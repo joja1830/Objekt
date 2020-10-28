12678d435664250d022b119b9d32cbfe6428de18
josefine jacobsen <josejaco@live.dk>
2020-10-28 10:12:23 +0100

    basic objekt programmering

---
+++ Objekt Orienteret Programmering/bedre_forståelse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bedre_forståelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
